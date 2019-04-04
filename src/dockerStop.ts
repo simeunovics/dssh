7d9412723033be9ba87d42047344fe702158571e
Simo Simeunovic <simo.simeunovic@esome.com>
2019-04-04 22:43:16 +0200

    Provide feedback

--- src/dockerStop.ts
+++ src/dockerStop.ts
@@ -2,7 +2,10 @@ import { shellExec } from "./helpers";
 
 export default async function dockerStop(): Promise<Boolean> {
   try {
+    console.log("🛑 Stopping all containers...");
     await shellExec("docker stop $(docker ps -a -q);");
+    console.log("");
+    console.log("✅ Done.");
     return true;
   } catch (e) {
     console.error(e);
