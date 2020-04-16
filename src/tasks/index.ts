2ac39bc690b5ea13ad2c705ba5e86661d82c37a0
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-16 23:06:39 +0200

    Update index.ts

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,15 +1,11 @@
-interface Task {
+interface ITask {
   displayText: string;
   callback: () => Promise<Boolean>;
 }
-import attachToRunningContainer from "./attachToRunningContainer";
-import dockerStop from "./dockerStop";
-import dockerNuke from "./dockerNuke";
+import attachToRunningContainer from './attachToRunningContainer';
+import dockerStop from './dockerStop';
+import dockerNuke from './dockerNuke';
 
-const tasks: Task[] = [
-  attachToRunningContainer,
-  dockerStop,
-  dockerNuke
-]
+const tasks: ITask[] = [attachToRunningContainer, dockerStop, dockerNuke];
 
 export default tasks;
