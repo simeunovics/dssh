14aa935918d6bdffe2fb2c55ffbce71fcfcd1d3f
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-16 22:48:58 +0200

    Update attachToRunningContainer.ts

--- src/tasks/attachToRunningContainer.ts
+++ src/tasks/attachToRunningContainer.ts
@@ -1,62 +1,43 @@
-import * as inquirer from "inquirer";
-import { spawn } from "child_process";
-import { shellExec } from "../helpers";
+import * as inquirer from 'inquirer';
+import listRunningContainers from '../TerminalCommands/ListRunningContainers';
+import attachToContainer from '../TerminalCommands/AttachToContainer';
 
-const BYE_MESSAGE = "Bye... 👋";
-const PICK_USER_QUESTION = "As user";
-const PICK_CONTAINER_QUESTION = "Attach to container";
-const DOCKER_PS_FORMAT = "--format '{{.ID}}\t{{.Names}}'";
+const BYE_MESSAGE = 'Bye... 👋';
+const PICK_USER_QUESTION = 'As user';
+const PICK_CONTAINER_QUESTION = 'Attach to container';
 
 const getRunningContainers = async () => {
-  const dockerContainers: string = await shellExec(
-    `docker ps ${DOCKER_PS_FORMAT}`
-  );
-
-  return dockerContainers
-    .trim()
-    .split("\n")
-    .filter(row => Boolean(row.length))
-    .map(row => {
-      const [id, name] = row.split("\t");
-
-      return {
-        id: id.trim(),
-        name: name.trim()
-      };
-    });
+  const terminalCommand = listRunningContainers();
+  return terminalCommand.execute();
 };
 
 async function attachToRunningContainer(): Promise<Boolean> {
   try {
     const runningContainers = await getRunningContainers();
+
     if (!Boolean(runningContainers.length)) {
-      console.info("No running containers found!");
+      console.info('No running containers found!');
       return;
     }
 
     const answers = await inquirer.prompt([
       {
-        type: "list",
+        type: 'list',
         name: PICK_CONTAINER_QUESTION,
-        choices: runningContainers.map(({ name }) => name)
+        choices: runningContainers.map(({ name }) => name),
       },
-      { type: "input", name: PICK_USER_QUESTION, default: "root" }
+      { type: 'input', name: PICK_USER_QUESTION, default: 'root' },
     ]);
 
     const containerId = runningContainers
       .filter(({ name }) => name === answers[PICK_CONTAINER_QUESTION])
       .reduce((next, prev) => ({ ...next, ...prev })).id;
 
-    const ssh = await spawn(
-      "docker",
-      `exec --user ${
-        answers[PICK_USER_QUESTION]
-        } -it ${containerId} bash`.split(" "),
-      {
-        stdio: "inherit"
-      }
-    );
-    ssh.on("exit", () => console.log(BYE_MESSAGE));
+    const user = answers[PICK_USER_QUESTION];
+    const command = attachToContainer({ containerId, user });
+
+    await command.execute();
+    console.log(BYE_MESSAGE);
 
     return true;
   } catch (e) {
@@ -66,6 +47,6 @@ async function attachToRunningContainer(): Promise<Boolean> {
 }
 
 export default {
-  displayText: "Attach to running container",
-  callback: attachToRunningContainer
-}
+  displayText: 'Attach to running container',
+  callback: attachToRunningContainer,
+};
