dff21524c91037bf3572cac0fb474638681c527d
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-17 16:52:33 +0200

    Remove invalid test

--- test/TerminalCommands/AttachToContainer.spec.ts
+++ test/TerminalCommands/AttachToContainer.spec.ts
@@ -1,17 +1,6 @@
 import createTerminal from '../../src/Services/Terminal';
 import createCommand from '../../src/TerminalCommands/AttachToContainer';
 
-test('it will list all currently running containers', async () => {
-  const command = createCommand({
-    user: 'root',
-    containerId: '123123123',
-  });
-
-  expect(await command.toString()).toEqual(
-    'docker exec --user root -it 123123123 bash'
-  );
-});
-
 test('it can attach to running container', async () => {
   const terminal = createTerminal();
   terminal.interactiveShell = jest.fn(async () => 'done');
