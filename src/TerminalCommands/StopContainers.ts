853680e6bcdde68f00b3bd7be1a51b43f535c303
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-17 21:06:38 +0200

    Export StopContainers class

--- src/TerminalCommands/StopContainers.ts
+++ src/TerminalCommands/StopContainers.ts
@@ -1,7 +1,7 @@
 import createTerminalInstance from '../Services/Terminal';
 import { ITerminalCommand, ITerminal } from '../Interfaces';
 
-class StopContainers implements ITerminalCommand {
+export class StopContainers implements ITerminalCommand {
   private command: string = 'docker stop $(docker ps -a -q)';
   public constructor(private terminal: ITerminal = terminal) {}
 
