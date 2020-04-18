957364f9ab2ac21d4c1dc4c0458c49c262eb0cd8
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-19 00:15:01 +0200

    Add file system service

---
+++ test/Services/FileSystem.spec.ts
@@ -0,0 +1,24 @@
+import { FileSystem } from '../../src/Services/FileSystem';
+
+test('it can list directory for files', async () => {
+  const path: any = jest.genMockFromModule('path');
+  path.isAbsolute = jest.fn(() => true);
+  path.join = jest.fn((a, b) => `${a}/${b}`);
+
+  const fs: any = jest.genMockFromModule('fs');
+  fs.readdir = (_: any, options: any, callback: any) => {
+    callback(null, [
+      { isDirectory: () => false, name: 'a' },
+      { isDirectory: () => false, name: 'b' },
+    ]);
+  };
+
+  const fileSystem = new FileSystem(fs, path);
+  const files = await fileSystem.listFiles('dummy-folder');
+
+  expect(files.length).toEqual(2);
+  expect(files).toEqual([
+    { absolutePath: 'dummy-folder/a' },
+    { absolutePath: 'dummy-folder/b' },
+  ]);
+});
