ef08a049a7266990865c099987be604018e69c2c
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-18 14:55:41 +0200

    Mock interactiveShell instead of execute method

--- test/TerminalCommands/RunContainer.spec.ts
+++ test/TerminalCommands/RunContainer.spec.ts
@@ -3,11 +3,13 @@ import { RunContainer } from '../../src/TerminalCommands/RunContainer';
 
 test('it can execute command', async () => {
   const terminal = createTerminal();
-  terminal.execute = jest.fn();
+  terminal.interactiveShell = jest.fn();
 
   const command = new RunContainer(terminal, 'node');
   await command.execute();
 
-  expect(terminal.execute).toBeCalledTimes(1);
-  expect(terminal.execute).toBeCalledWith('docker run -it node /bin/bash');
+  expect(terminal.interactiveShell).toBeCalledTimes(1);
+  expect(terminal.interactiveShell).toBeCalledWith(
+    'docker run -it node /bin/bash'
+  );
 });
