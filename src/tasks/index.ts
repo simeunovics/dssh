e992145681209a742bcb6b778ccbf1400e622cd5
Simo Simeunovic <simo.simeunovic@esome.com>
2020-04-19 14:47:53 +0200

    Update index.ts

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -6,12 +6,14 @@ import attachToRunningContainer from './attachToRunningContainer';
 import dockerStop from './dockerStop';
 import dockerNuke from './dockerNuke';
 import runContainer from './runContainer';
+import runDockerCompose from './runDockerCompose';
 
 const tasks: ITask[] = [
   attachToRunningContainer,
   dockerStop,
   dockerNuke,
   runContainer,
+  runDockerCompose,
 ];
 
 export default tasks;
